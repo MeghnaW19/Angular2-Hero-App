f645856a9e132e9429540bf692523fe1208e1564
Meghna <meghnanvt70@gmail.com>
2017-05-16 14:53:02 +0530

    Authentication performed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroesComponent }      from './heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
+import { LoginComponent }  from './login.component';
+import { AppComponent }  from './app.component';
 
 const routes: Routes = [
- { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+ { path: '', redirectTo: '/login', pathMatch: 'full' },
  { path: 'dashboard',  component: DashboardComponent },
  { path: 'detail/:id', component: HeroDetailComponent },
- { path: 'heroes',     component: HeroesComponent }
+ { path: 'heroes',     component: HeroesComponent },
+ { path: 'app-root',     component:AppComponent }
+
 ];
 @NgModule({
  imports: [ RouterModule.forRoot(routes) ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,65 +1,38 @@
-import { NgModule }             from '@angular/core';
-import { BrowserModule }        from '@angular/platform-browser';
-import { FormsModule }          from '@angular/forms'; //ngModel lives here
-import { RouterModule }         from '@angular/router';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {  ReactiveFormsModule ,FormsModule}   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 import { AppComponent }         from './app.component';
-import { HeroDetailComponent }  from './hero-detail.component';
-import { HeroesComponent }      from './heroes.component';
-import { HeroService }          from './hero.service';
 import { DashboardComponent }   from './dashboard.component';
-import { AppRoutingModule }     from './app-routing.module';
-import { HttpModule }           from '@angular/http';
+import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }          from './hero.service';
 import { HeroSearchComponent }  from './hero-search.component';
-import {Authentication}         from './authentication';
-import {loginComponent}         from  './login';
-
+import { LoginComponent }  from './login.component';
+import { Authentication } from './authentication';
+import { Logout } from './logout';
 
 @NgModule({
-  imports: [
-           BrowserModule,
-           FormsModule, // import the FormsModule before binding with [(ngModel)]
-           HttpModule,
-           InMemoryWebApiModule.forRoot(InMemoryDataService),
-           AppRoutingModule,
-
-  RouterModule.forRoot([
-     {
-           path: 'heroes', //router matches this route's path to the URL in the browser address bar (heroes).
-           component: HeroesComponent
-     },
-     {
-           path: 'dashboard',
-           component: DashboardComponent
-     },
-     {
-           path: '',
-           redirectTo: '/dashboard',
-           pathMatch: 'full'
-      },
-      {
-           path: 'detail/:id',
-           component: HeroDetailComponent
-      },
-   ]),
+ imports: [
+   BrowserModule,
+    ReactiveFormsModule,FormsModule,
+   HttpModule,
+   InMemoryWebApiModule.forRoot(InMemoryDataService),
+   AppRoutingModule
  ],
-  
-  declarations: [
-           AppComponent,
-           DashboardComponent,
-           HeroDetailComponent,
-           HeroesComponent,
-           HeroSearchComponent
+ declarations: [
+   AppComponent,
+   DashboardComponent,
+   HeroDetailComponent,
+   HeroesComponent,
+   HeroSearchComponent,
+   LoginComponent,Logout
  ],
-  
-  providers: [
-           HeroService
- ],
- 
-  bootstrap: [ AppComponent ]
+ providers: [ HeroService,Authentication ],
+ bootstrap: [ LoginComponent ]
 })
-
-export class AppModule {
-}
+export class AppModule { }
